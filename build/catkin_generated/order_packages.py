3da1f8a5e9a51ce0caf63ecb21d9c9f14d3cf0c8
americast <sayan.sinha@iitkgp.ac.in>
2017-02-23 16:38:33 +0530

    Made

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = "/home/ss/catkin_ws/src"
 whitelisted_packages = "".split(';') if "" != "" else []
 blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/ss/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/home/ss/catkin_ws/devel;/opt/ros/kinetic" != "" else []
+underlay_workspaces = "/home/ss/catkin2/devel;/home/ss/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/home/ss/catkin2/devel;/home/ss/catkin_ws/devel;/opt/ros/kinetic" != "" else []
--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -8,4 +8,3 @@
 
 # modified environment variables
 export PWD="/home/ss/catkin_ws/build"
-export PYTHONPATH="/home/ss/catkin_ws/devel/lib/python2.7/dist-packages:$PYTHONPATH"
